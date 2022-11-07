e1812dc97cef4da87642cdbe43f9538e2f4fdc8c
Pluto <wyyuyongsong@163.com>
2022-11-07 21:23:55 +0800

    2

--- ssm/src/main/java/com/yuyongsong/ssm/controller/FaceController.java
+++ ssm/src/main/java/com/yuyongsong/ssm/controller/FaceController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -22,15 +22,16 @@ public class FaceController {
     @Autowired
     private FaceService faceService;
 
-    @RequestMapping("img")
-    public String img(@RequestParam("fileName" ) MultipartFile file ){
-        ModelAndView mav = new ModelAndView();
 
-        String username =null;
+    @RequestMapping("/img.do")
+    public ModelAndView img(@RequestParam("fileName" ) MultipartFile file,HttpSession session){
+
+
+        String username =(String) session.getAttribute("user");
 
         System.out.println(username);
-        String fileName = FaceSpot.saveFile(file,"D:\\yuyongsong\\zhaopian");
-
+        String fileName = FaceSpot.saveFile(file,"D:\\apply\\apache-tomcat-9.0.68\\webapps\\img");
+        ModelAndView modelAndView = new ModelAndView();
         if(fileName!=null){
             byte[] byface = FaceSpot.FileToByte(file);
 
@@ -52,17 +53,41 @@ public class FaceController {
                     face.setBeauty(beautyOne);
 
                     String gender = faceV3DetectBean.getResult().getFace_list().get(i).getGender().getType();
-                    face.setGender(gender);
+                    if(gender.equals("male")){
+                        face.setGender("男");
+                    }else if(gender.equals("female")){
+                        face.setGender("女");
+                    }
+
 
                     String glasses = faceV3DetectBean.getResult().getFace_list().get(i).getGlasses().getType();
-                    face.setGlasses(glasses);
+                    if(glasses.equals("none")){
+                        face.setGlasses("没有戴眼镜");
+                    }else if(glasses.equals("sun")){
+                        face.setGlasses("带了墨镜");
+                    }else if(glasses.equals("common")){
+                        face.setGlasses("带了普通眼镜");
+                    }
+
 
                     String expression = faceV3DetectBean.getResult().getFace_list().get(i).getExpression().getType();
-                    face.setExpression(expression);
-                    face.setImgPath("D:\\yuyongsong\\zhaopian"+fileName);
+                    if(expression.equals("none")){
+                        face.setExpression("不笑");
+                    }else if(expression.equals("smile")){
+                        face.setExpression("微笑");
+                    }else if(expression.equals("laugh")){
+                        face.setExpression("大笑");
+                    }
+
+                    face.setImgPath("http://localhost:8080/img/"+fileName);
 
                 }
                 faceService.insertFact(face);
+                List<Face> list = faceService.selectFactAll();
+                modelAndView.addObject("faces",list);
+                modelAndView.addObject("user",username);
+                modelAndView.setViewName("index");
+
             }else {
                 System.out.println("百度人脸检测有误");
             }
@@ -70,17 +95,9 @@ public class FaceController {
             System.out.println("请上传一张照片");
         }
 
-        return "redirect:/selectface";
-    }
-
-    @RequestMapping("selectface")
-    public ModelAndView selectface() throws Exception {
-        List<Face> list = faceService.selectFactAll();
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("faces",list);
-        modelAndView.setViewName("index");
         return modelAndView;
     }
+
+
 
 }
--- ssm/src/main/java/com/yuyongsong/ssm/controller/UserController.java
+++ ssm/src/main/java/com/yuyongsong/ssm/controller/UserController.java
@@ -4,11 +4,14 @@ import com.yuyongsong.ssm.pojo.User;
 import com.yuyongsong.ssm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -23,11 +26,10 @@ public class UserController {
         ModelAndView mav = new ModelAndView();
         try{
             userService.insertUser(user);
-            mav.addObject("success","/login");
             mav.addObject("msg","注册成功、前往登录");
 
         }catch (DataAccessException e){
-            mav.addObject("crash","用户名已被注册");
+            mav.addObject("msg","用户名已被注册");
 
         }
 
@@ -46,15 +48,15 @@ public class UserController {
     }
 
     @RequestMapping(value = "/login.do",method = RequestMethod.POST)
-    public ModelAndView loginDo(User user){
+    public ModelAndView loginDo(User user,HttpSession session){
+        System.out.println(user.toString());
         User user1 = userService.selectUser(user);
         ModelAndView mav = new ModelAndView();
         if(user1==null){
-            System.out.println("账号或者密码错误");
             mav.addObject("svm","账号或者密码错误");
-            System.out.println("账号或者密码错误");
             mav.setViewName("login");
         }else {
+            session.setAttribute("user",user1.getUsername());
             mav.addObject("user",user.getUsername());
             mav.setViewName("index");
         }
